959bce4b27d09f25ded7d4f0fabace071e1870d6
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-09-19 23:09:16 +0600

    added the jobCard

--- client/src/Components/TabCatagory/TabCatagory.jsx
+++ client/src/Components/TabCatagory/TabCatagory.jsx
@@ -1,13 +1,21 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import JobCard from "../JobCard/JobCard";
 
 const TabCatagory = () => {
   return (
     <div className="mt-20 mb-40">
-     <div className="flex flex-col items-center justify-center gap-4 mb-16">
-          <h1 className="text-4xl  font-bold text-center">Browse Job By Catagory</h1>
-          <p className=" text-xl font-medium text-center w-auto md:w-[750px]">Browse through all the jobs currently advertised on Job Bank by location, category or employer. Top searched job titles.Find job openings hiring now - browse millions of jobs by Category hiring now on ZipRecruiter.</p>
-     </div>
+      <div className="flex flex-col items-center justify-center gap-4 mb-16">
+        <h1 className="text-4xl  font-bold text-center">
+          Browse Job By Catagory
+        </h1>
+        <p className=" text-xl font-medium text-center w-auto md:w-[750px]">
+          Browse through all the jobs currently advertised on Job Bank by
+          location, category or employer. Top searched job titles.Find job
+          openings hiring now - browse millions of jobs by Category hiring now
+          on ZipRecruiter.
+        </p>
+      </div>
 
       <Tabs>
         <div className="w-full border-b ">
@@ -33,15 +41,26 @@ const TabCatagory = () => {
           </TabList>
         </div>
 
-        <TabPanel>
-          <h2>Any content 1</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 3</h2>
-        </TabPanel>
+        <div className="mt-8">
+          <TabPanel>
+            <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8">
+              <JobCard></JobCard>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8">
+              <JobCard></JobCard>
+              <JobCard></JobCard>
+              <JobCard></JobCard>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8">
+              <JobCard></JobCard>
+              <JobCard></JobCard>
+            </div>
+          </TabPanel>
+        </div>
       </Tabs>
     </div>
   );
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import SweiperSlider from "./SweiperSlider";
 
 const Home = () => {
      return (
-          <div className="w-full px-4 md:px-12 lg:px-20">
+          <div className="w-full mt-2 px-4 md:px-12 lg:px-20">
                <SweiperSlider></SweiperSlider>
                <TabCatagory></TabCatagory>
           </div>
