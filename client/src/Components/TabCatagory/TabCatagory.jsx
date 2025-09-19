3c2e32f684ee41179a4b4ed288b4a1441e6b540a
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-09-19 23:01:05 +0600

    added the browse job

--- client/src/Components/TabCatagory/TabCatagory.jsx
+++ client/src/Components/TabCatagory/TabCatagory.jsx
@@ -3,18 +3,44 @@ import "react-tabs/style/react-tabs.css";
 
 const TabCatagory = () => {
   return (
-    <div>
+    <div className="mt-20 mb-40">
+     <div className="flex flex-col items-center justify-center gap-4 mb-16">
+          <h1 className="text-4xl  font-bold text-center">Browse Job By Catagory</h1>
+          <p className=" text-xl font-medium text-center w-auto md:w-[750px]">Browse through all the jobs currently advertised on Job Bank by location, category or employer. Top searched job titles.Find job openings hiring now - browse millions of jobs by Category hiring now on ZipRecruiter.</p>
+     </div>
+
       <Tabs>
-        <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
-        </TabList>
+        <div className="w-full border-b ">
+          <TabList className="flex items-center justify-center gap-6">
+            <Tab
+              className="px-4 py-2 cursor-pointer focus:outline-none"
+              selectedClassName="border-t border-l border-r  rounded-t-md bg-background text-blue-600 font-semibold -mb-[1px]"
+            >
+              Web Development
+            </Tab>
+            <Tab
+              className="px-4 py-2 cursor-pointer focus:outline-none"
+              selectedClassName="border-t border-l border-r  rounded-t-md bg-background text-blue-600 font-semibold -mb-[1px]"
+            >
+              Graphic Design
+            </Tab>
+            <Tab
+              className="px-4 py-2 cursor-pointer focus:outline-none"
+              selectedClassName="border-t border-l border-r  rounded-t-md bg-background text-blue-600 font-semibold -mb-[1px]"
+            >
+              Digital Marketing
+            </Tab>
+          </TabList>
+        </div>
 
         <TabPanel>
           <h2>Any content 1</h2>
         </TabPanel>
         <TabPanel>
           <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
         </TabPanel>
       </Tabs>
     </div>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import TabCatagory from "../../Components/TabCatagory/TabCatagory";
 import SweiperSlider from "./SweiperSlider";
 
 const Home = () => {
      return (
           <div className="w-full px-4 md:px-12 lg:px-20">
                <SweiperSlider></SweiperSlider>
+               <TabCatagory></TabCatagory>
           </div>
      );
 };
