92f941c99a7404b0ca1e04fee062301537a62f4a
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-09-23 18:02:22 +0600

    added the complete

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // ---------- CORS CONFIG ----------
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://solosphere-da657.web.app", "https://solosphere-da657.firebaseapp.com"],
   credentials: true, // ✅ must be lowercase
   optionSuccessStatus: 200,
 };
