e977458bb2474d838d316d4c24351f604e00d16b
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-13 14:06:29 +0600

    added the update index.js

--- server/index.js
+++ server/index.js
@@ -8,25 +8,16 @@ const client = require("./src/helper/client");
 const jobRoutes = require("./src/routes/jobroutes");
 const bidRoutes = require("./src/routes/bidroutes");
 
+//Import Middleware
+const verifyToken = require("./src/middleware/verifyToken");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// ----------- MIDDLEWARES -----------
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
-// ----------- JWT VERIFY -----------
-const verify = (req, res, next) => {
-  const token = req.cookies?.token;
-  if (!token) return res.status(401).send({ message: "Unauthorized (no token)" });
-
-  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
-    if (err) return res.status(401).send({ message: "Invalid token" });
-    req.user = decoded;
-    next();
-  });
-};
 
 // ----------- JWT CREATE -----------
 app.post("/jwt", async (req, res) => {
@@ -54,6 +45,7 @@ app.get("/log-out", (req, res) => {
 });
 
 // ----------- ROUTES -----------
+
 app.use("/jobs", jobRoutes);
 app.use("/bids", bidRoutes);
 
@@ -69,4 +61,4 @@ app.use((req, res) => {
 });
 
 // ----------- SERVER RUN -----------
-app.listen(port, () => console.log(`solosphere Server running on port ${port}`));
+app.listen(port, () => console.log(`Solosphere Server running on port ${port}`));
