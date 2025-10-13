ca61831d5f7b62da4c0ab65a90afe6f91502e8e3
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-13 12:54:12 +0600

    added the jobroutes

---
+++ server/src/routes/jobroutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getJobs,
+  getJobById,
+  getJobsByEmail,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/JobController");
+
+const router = express.Router();
+
+router.get("/", getJobs);
+router.get("/:id", getJobById);
+router.get("/email/:email", getJobsByEmail);
+router.post("/", createJob);
+router.put("/:id", updateJob);
+router.delete("/:id", deleteJob);
+
+module.exports = router;
